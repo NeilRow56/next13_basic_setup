ee96b9e8b5bc2b13b956a8e6ec9303601ea6bbe2
Neil <neilrowland56@yahoo.com>
2022-11-30 09:04:05 +0000

    first commit

---
+++ app/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex bg-gray-600 h-12 items-center text-white">
+      <div className="w-[800px] mx-auto flex justify-center">
+        <div className="first_link"> Footer One</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ app/header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex bg-gray-600 h-12 items-center text-white">
+      <div className="w-[800px] mx-auto flex justify-between">
+        <div className="first_link">One</div>
+        <div className="second_link">Two</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
